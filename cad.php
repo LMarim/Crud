c762f28d4bb055c349b19722631839adb19e1b6c
Lucas Marinho <128625338+LMarim@users.noreply.github.com>
2024-08-14 16:34:55 -0300

    Add files via upload

---
+++ cad.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = $_POST['name'];
+$escolaridade = $_POST['escolaridade'];
+$gender = $_POST['gender'];
+$comment = $_POST['comment'];
+
+echo("Nome: " . $name .'<br>');
+
+echo("Escolaridade: " . $escolaridade .'<br>');
+
+echo("Genero: " . $gender .'<br>');
+
+echo("Comentario:" . $comment);
+
+
+
+?>
